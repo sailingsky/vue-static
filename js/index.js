ba02733a7e57d9f3cc29cb8788035d1baa8c4237
zhangshuangjian <zhangshuangjian@huoyunren.com>
2016-09-28 18:21:42 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-var example = {
-    name:"beck"
+var data = {
+    name:"beck",
+    message:"this is a message",
+    arrays:[{text:"text 1 "},{text:'text 2'},{text:'text 4'}]
 };
 
 var vue = new Vue({
    el:'#example-1',
-    data:example
+   data:data,
+   methods:{
+       alert:function(){
+           alert("alert !!");
+       }
+   }
 });
